57636bdf0db6ebd5cf3f52f12fe7c868131aefee
minato0207 <kota0207xx@icloud.com>
2021-04-04 14:53:16 +0900

    Initial Commit

---
+++ LeisureApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeisureApp
+//
+//  Created by 湊航太 on 2021/04/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
